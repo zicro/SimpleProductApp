710abc885b0eddb78611509dfe0f36732af61368
Omae <larhnimikit@gmail.com>
2023-07-04 07:06:29 +0100

    main code

---
+++ backend/routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::resource('products', ProductController::class);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
